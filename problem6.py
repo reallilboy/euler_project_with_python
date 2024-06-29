dfc2d886cf1e5c4b83f7313443d88c5175f94dab
lilboy <166255379+reallilboy@users.noreply.github.com>
2024-06-29 17:14:04 +0330

    Add files via upload

--- problem6.py
+++ problem6.py
@@ -1,31 +1,10 @@
-#Considering a room with 100 people in it
-#We want to do a lottery,
-#then people randomly give money to each
-#other until one person's money reaches 0
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
 
-
-
-import random
-random.seed()
-
-
-
-#We create 100 people using this loop
-people = []
-for i in range(50):
-    people.append(100)
-
-#we create lottery among people this loop
-for x in range(1000):
-    for person1 in range(50): 
-
-#Here we choose people randomly
-        person2 = random.randrange(50)
-        while person2 == 0:
-            person2 = random.randrange(50)
-#We check people's money
-        if people[person1] != 0:
-            people[person1] -= 1
-            people[person2] += 1
-
-print(people)
+num = 2
+while True:
+    if all(num % n == 0 for n in range(1,21)):
+        print(num)
+        break
+    else:
+        num += 2
